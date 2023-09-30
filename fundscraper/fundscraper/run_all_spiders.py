78a852e334adb52b15b28e87267b51ab4556a1e7
manavkdubey <dmanavkumar24@gmail.com>
2023-10-01 02:44:32 +0530

    Feat(requirements):Add requirements.txt

--- fundscraper/fundscraper/run_all_spiders.py
+++ fundscraper/fundscraper/run_all_spiders.py
@@ -13,4 +13,4 @@ while True:
             print(f"Error running {spider_name}: {e}")
 
     # Wait for 5 minutes before running again
-    time.sleep(5)  # 300 seconds = 5 minutes
+    time.sleep(300)  # 300 seconds = 5 minutes
